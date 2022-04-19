c9bcfa8f457948dfcde1dc25f9dd2b663d593e63
unknown <pavlina.itstep.student@gmail.com>
2022-04-19 16:47:09 +0300

    Delete Student

--- Lesson 5/Lesson5.py
+++ Lesson 5/Lesson5.py
@@ -11,12 +11,22 @@ class Student:
     def show(self):
         print(self.name)
         print(self.health)
-        print(self.lvl)   
+        print(self.lvl)
+    # How to Delete Student
+    # Як видалити студента.
+    def __del__(self):
+        print("Student Deleted!")
+
+
 
 First = Student(name=input("Enter Name: "),health=10)
 First.show()
 
 Second = Student(name="Kate",health=43)
+Second.show()
+
+Second.__del__()
+
 Second.show()
 
 print(Student.amount_students)
